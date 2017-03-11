155aaf8d381a3394100d16f924273f771c0a49b8
Eetu Sutinen <eetu.sutinen@gmail.com>
2017-03-11 19:18:53 +0200

    new relic support

--- project.clj
+++ project.clj
@@ -5,11 +5,22 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-jetty-adapter "1.5.1"]
-                 [compojure "1.5.2"]]
-  :plugins [[lein-ring "0.11.0"]]
+                 [environ "1.1.0"]
+                 [ring/ring-core "1.6.0-RC1"]
+                 [ring/ring-devel "1.6.0-RC1"]
+                 [ring/ring-jetty-adapter "1.6.0-RC1"]
+                 [compojure "1.6.0-beta3"]
+                 [yleisradio/new-reliquary "1.0.0"]]
+  :plugins [[lein-ring "0.11.0"]
+            [lein-environ "1.1.0"]
+            [com.pupeno/jar-copier "0.4.0"]]
+  :prep-tasks ["javac" "compile" "jar-copier"]
+  :jar-copier {:java-agents true
+               :destination "resources/jars"}
   :ring {:handler bush-backend.core/application}
   :main ^:skip-aot bush-backend.core
   :repl-options {:init-ns bush-backend.core}
   :uberjar-name "bush-backend.jar"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:dev {:dependencies []}
+             :uberjar {:aot :all}}
+  :java-agents [[com.newrelic.agent.java/newrelic-agent "3.36.0"]])
--- src/bush_backend/core.clj
+++ src/bush_backend/core.clj
@@ -1,6 +1,10 @@
 (ns bush-backend.core
   (:require [compojure.core :refer [defroutes GET]]
+            [environ.core :refer [env]]
+            [new-reliquary.ring :refer [wrap-newrelic-transaction]]
             [ring.adapter.jetty :as ring]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.reload :refer [wrap-reload]]
             [compojure.route :as route]
             [compojure.handler :as handler])
   (:gen-class))
@@ -8,7 +12,14 @@
 (defroutes routes
   (GET "/" [] "Hello world!"))
 
-(def application (handler/site routes))
+(def application
+  (let [handler (handler/site routes)]
+    (if (env :dev)
+      (-> handler
+          wrap-reload)
+      (-> handler
+          wrap-params
+          wrap-newrelic-transaction))))
 
 (defn start [port]
   (ring/run-jetty application {:port port
