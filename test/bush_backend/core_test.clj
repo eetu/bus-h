cb322a0272a16481fdc6829ca2e97e82f786844d
Eetu Sutinen <eetu.sutinen@gmail.com>
2017-03-12 12:11:50 +0200

    simple test for endpoint

--- test/bush_backend/core_test.clj
+++ test/bush_backend/core_test.clj
@@ -1,7 +1,8 @@
 (ns bush-backend.core-test
   (:require [clojure.test :refer :all]
-            [bush-backend.core :refer :all]))
+            [bush-backend.core :refer :all]
+            [ring.mock.request :as mock]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest application-test
+  (let [response (application (mock/request :get "/bush/action/next-arrival-to-nearest-stop"))]
+    (is (= (:status response) 200))))
